cd433995152ad3f78630fe6abe45c07fe0a717f5
saurabh <saurabhshinde9977@gmail.com>
2026-03-10 23:49:20 +0530

    commit this file

--- count_digit_dsa.py
+++ count_digit_dsa.py
@@ -1,10 +1,10 @@
-# def count_digits(n):
-#     count=0
-#     while n>0:
-#         count+=1
-#         n=n//10
-#     return count
-# print(count_digits(12345))
+def count_digits(n):
+    count=0
+    while n>0:
+        count+=1
+        n=n//10
+    return count
+print(count_digits(12345))
 
 from numpy import log10
 
